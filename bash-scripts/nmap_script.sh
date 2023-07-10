f0011d009340ff1796e38ecb1757a557d4ff4bb3
iamahmadpk <iamahmadpk53@gmail.com>
2023-07-10 18:20:13 +0500

    udated version fo nmap script

--- bash-scripts/nmap_script.sh
+++ bash-scripts/nmap_script.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
 
+# Get user input for cron time
+read -p "Enter the cron schedule (like */5 * * * * for every 5 minutes): " cron_schedule
+
+# Get user input for log file path and name
+read -p "Enter the path and name for the log file (like /home/ebryx/logfile.log): " log_file
+
 # Check if nmap is installed
 if ! command -v nmap &> /dev/null; then
     echo "Installing nmap..."
@@ -8,12 +14,12 @@ if ! command -v nmap &> /dev/null; then
 fi
 
 # Create a log file
-LOG_FILE="/home/ebryx/logfile.log"
-touch $LOG_FILE
+#LOG_FILE="/home/ebryx/logfile.log"
+touch $log_file
 
 # Set up cron job
-#CRON_CMD="*/5 * * * * /usr/bin/nmap -sS >> $LOG_FILE"
-#(crontab -l ; echo "$CRON_CMD") | crontab -
+CRON_CMD="$cron_schedule /usr/bin/nmap -sS >> $log_file"
+(crontab -l ; echo "$CRON_CMD") | crontab -
 
 echo "Cron job set up successfully."
 
